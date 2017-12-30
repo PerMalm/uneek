4d72ee81c0a61af2c10d61591e693e55c36329a2
Dan Rosén <dan.rosen@gu.se>
2017-12-30 13:49:44 +0100

    Add an amazing navbar from bootstrap

--- src/App.ts
+++ src/App.ts
@@ -6,6 +6,12 @@ import { Store, Lens, Omit } from "reactive-lens"
 import { tags, tag, s, TagData } from "snabbis"
 const {div, span, h1} = tags
 
+export const html = (innerHTML: string) => tag('div', s.props({ innerHTML }))
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+// import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
 export interface State {
   readonly a: string,
   readonly b: string,
@@ -25,7 +31,6 @@ export const App = (store: Store<State>) => {
   global.store = store
   global.reset = () => store.set(init)
   store.storage_connect('uneek')
-  store.on(x => console.log(JSON.stringify(x, undefined, 2)))
   return () => View(store)
 }
 
@@ -61,31 +66,85 @@ export const View = (store: Store<State>): VNode => {
             tag('span.w60.c', k, a > 0 && b > 0 && tag('span.small', ` (${a + b})`)),
             tag('span.w20.l', b > 0 && b))))
 
-  return tag(
-    '.rows.w100.h100.marginalized',
-    tag('.w20',
-      tag('.cols.h100.centered.vcentered',
-        s.textarea(a, undefined, undefined, s.classed('textarea')),
-        a_err && tag('.error', a_err)
-      )),
-    tag('.w60.cols.h100',
-      tag('.rows.centered.marginalized',
-        keys.map(
-          k =>
-            s.button(
-              () => store.at('key').set(k),
-              k,
-              s.attrs({disabled: k === store.at('key').get()})))),
-      tag('.rows.h100.marginalized',
-        count(only((a, b) => b == 0)),
-        count(only((a, b) => a > 0 && b > 0)),
-        count(only((a, b) => a == 0)),
-      )
-    ),
-    tag('.w20',
-      tag('.cols.h100.centered.vcentered',
-        s.textarea(b, undefined, undefined, s.classed('textarea')),
-        b_err && tag('.error', b_err)
-      )),
-  )
+  return tag('div.cols.h100',
+    html(`
+<nav class="navbar navbar-default navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>`),
+    tag('.rows.w100.h100.marginalized',
+      tag('.w20',
+        tag('.cols.h100.centered.vcentered',
+          s.textarea(a, undefined, undefined, s.classed('textarea')),
+          a_err && tag('.error', a_err)
+        )),
+      tag('.w60.cols.h100',
+        tag('.rows.centered.marginalized',
+          keys.map(
+            k =>
+              s.button(
+                () => store.at('key').set(k),
+                k,
+                s.attrs({disabled: k === store.at('key').get()})))),
+        tag('.rows.h100.marginalized',
+          count(only((a, b) => b == 0)),
+          count(only((a, b) => a > 0 && b > 0)),
+          count(only((a, b) => a == 0)),
+        )
+      ),
+      tag('.w20',
+        tag('.cols.h100.centered.vcentered',
+          s.textarea(b, undefined, undefined, s.classed('textarea')),
+          b_err && tag('.error', b_err)
+        )),
+  ))
 }
